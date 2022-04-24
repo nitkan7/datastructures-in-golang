accf703e1647dae3879cd4505ab95caa6050eb46
nitkan7 <57367051+nitkan7@users.noreply.github.com>
2022-04-24 23:00:31 +0530

    Update CompleteSinglyLinkedList.go

--- CompleteSinglyLinkedList.go
+++ CompleteSinglyLinkedList.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
+//represents the individual node of a linked list.
 type LinkedList struct{
-  value int
-  next *LinkedList
+  value int //represents the value held by the node
+  next *LinkedList //represents the next node it points to
 }
 
 func main(){
@@ -18,6 +19,8 @@ func main(){
   start.DeleteAtIndex(1)
   start.display()
 }
+
+//performs adding a new node a the head operation.
 func (head *LinkedList) AddAtHead(value int)*LinkedList{
   node:=LinkedList{value,nil}
   node.next=head
@@ -25,6 +28,7 @@ func (head *LinkedList) AddAtHead(value int)*LinkedList{
   return head
 }
 
+//performs adding new node at the end of the list.
 func (head *LinkedList) AddAtTail(value int){
   node:=LinkedList{value,nil}
   for ;head.next!=nil; {
@@ -33,12 +37,15 @@ func (head *LinkedList) AddAtTail(value int){
   head.next=&node
 }
 
+//prints the elements of the linked list.
 func (head *LinkedList) display(){
   for ;head!=nil; {
     fmt.Println(*head)
     head=head.next
   }
 }
+
+//performs addition of node before a specified index
 func (head *LinkedList) AddAtIndex(where,value int){
   node :=LinkedList{value,nil}
   for index := 0; index < where-1; index++ {
@@ -47,6 +54,8 @@ func (head *LinkedList) AddAtIndex(where,value int){
   node.next=head.next
   head.next=&node
 }
+
+//performs deletion of a node at the given index.
 func (head *LinkedList) DeleteAtIndex(where int){
   for index := 0; index < where-1; index++ {
     head=head.next
